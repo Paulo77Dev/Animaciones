6acc16297aa6165258a26cfd2936ac988c3b4112
Paulo77Dev <paulomadson16@gmail.com>
2024-11-15 12:19:16 -0300

    Alteración y insercción de rutas en el App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ChangeColorButton from './components/ChangeColorButton';
+import ToggleFadeButton from './components/ToggleFadeButton';
+import GSAPAnimation from './components/GSAPAnimation';
+import AnimeJSAnimation from './components/AnimeJSAnimation';
+import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Manipulação e Animações com React</h1>
+      <ChangeColorButton />
+      <ToggleFadeButton />
+      <GSAPAnimation />
+      <AnimeJSAnimation />
     </div>
   );
-}
+};
 
 export default App;
